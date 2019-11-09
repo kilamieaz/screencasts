bc1fee2ca4e9840cf10d440ed7f15ffc2a8d16e2
Sultan <im.sultaan05@gmail.com>
2019-11-09 22:21:14 +0700

    video watch page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,50 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<h1>Videos</h1>
+		<div class="video-container">
+			<div v-for="(video, index) in videos" :key="index">
+				<router-link :to="{ name: 'video-watch' , params: {id: video.id} }">
+					<div class="video-box">
+						<img :src="video.thumbnail" alt />
+						<div>
+							<h3>{{ video.name }}</h3>
+							<div v-html="video.description"></div>
+						</div>
+					</div>
+				</router-link>
+			</div>
+		</div>
 	</div>
 </template>
 
  <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 export default {
 	name: "home",
-	components: {
-		HelloWorld
+	components: {},
+	data() {
+		return {
+			videos: this.$store.state.videos
+		};
 	}
 };
 </script>
+
+<style scoped lang="scss">
+.video-container {
+	.video-box {
+		border: 1px solid;
+		border-radius: 10px;
+		margin: 10px;
+		padding: 10px;
+		text-align: left;
+
+		display: flex;
+		justify-content: flex-start;
+
+		img {
+			width: 200px;
+			padding: 10px;
+		}
+	}
+}
+</style>
