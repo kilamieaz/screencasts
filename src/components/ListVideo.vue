0779c255ee7c7a4cceb42664834da3b6c0c306b5
Sultan <im.sultaan05@gmail.com>
2019-11-14 22:04:42 +0700

    showing played status on video lists

--- src/components/ListVideo.vue
+++ src/components/ListVideo.vue
@@ -3,6 +3,16 @@
 		<v-card width="340px" hover class="ma-2" :to="{ name: 'video-watch' , params: {id: video.id} }">
 			<v-img :src="video.thumbnail" alt />
 			<v-card-title>{{ video.name }}</v-card-title>
+			<v-card-text>
+				<div class="green--text" v-if="isPlayed">
+					<v-row>
+						<v-col cols="1">
+							<v-icon class="green--text" small>fas fa-check</v-icon>
+						</v-col>
+						<v-col cols="11">Played</v-col>
+					</v-row>
+				</div>
+			</v-card-text>
 			<v-card-actions v-if="video.tags">
 				<span v-for="tag in video.tags" :key="tag.id">
 					<v-btn
@@ -21,6 +31,11 @@
 
 <script>
 export default {
+	computed: {
+		isPlayed() {
+			return this.$store.state.playedVideos.includes(this.video.id);
+		}
+	},
 	props: ["video"]
 };
 </script>
