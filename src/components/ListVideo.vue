4a5a6223abbd75b0902458090e6b8064233106b1
Sultan <im.sultaan05@gmail.com>
2019-11-14 16:49:36 +0700

    vuetify: video list

--- src/components/ListVideo.vue
+++ src/components/ListVideo.vue
@@ -1,19 +1,20 @@
 <template>
-	<div>
-		<router-link :to="{ name: 'video-watch' , params: {id: video.id} }">
-			<div class="video-box">
-				<img :src="video.thumbnail" alt />
-				<div>
-					<h3>{{ video.name }}</h3>
-					<div v-html="video.description"></div>
-					<span v-for="tag in video.tags" :key="tag.id">
-						<router-link :to="{ name: 'tag', params: { id: tag.id }}">
-							<button class="tag-button">{{ tag.name }}</button>
-						</router-link>
-					</span>
-				</div>
-			</div>
-		</router-link>
+	<div v-if="video">
+		<v-card width="340px" hover class="ma-2" :to="{ name: 'video-watch' , params: {id: video.id} }">
+			<v-img :src="video.thumbnail" alt />
+			<v-card-title>{{ video.name }}</v-card-title>
+			<v-card-actions v-if="video.tags">
+				<span v-for="tag in video.tags" :key="tag.id">
+					<v-btn
+						color="green lighten-2"
+						class="mr-2"
+						small
+						@mousedown.stop
+						:to="{ name: 'tag', params: { id: tag.id }}"
+					>{{ tag.name }}</v-btn>
+				</span>
+			</v-card-actions>
+		</v-card>
 	</div>
 </template>
 
@@ -25,18 +26,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.video-box {
-	border: 1px solid;
-	border-radius: 10px;
-	margin: 10px;
-	padding: 10px;
-	text-align: left;
-	display: flex;
-	justify-content: flex-start;
-
-	img {
-		width: 200px;
-		padding: 10px;
-	}
-}
 </style>
