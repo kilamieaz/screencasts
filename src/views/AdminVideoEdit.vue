91bc020cc2ff645a3252d21ed7d1146dacc6786f
Sultan <im.sultaan05@gmail.com>
2019-11-20 10:51:52 +0700

    video form component

--- src/views/AdminVideoEdit.vue
+++ src/views/AdminVideoEdit.vue
@@ -1,54 +1,15 @@
 <template>
 	<v-container v-if="video">
-		<v-form v-model="valid">
-			<v-text-field
-				v-model="video.name"
-				label="Name"
-				counter="50"
-				:rules="[required('name'), minLength('name', 5), maxLength('name', 50)]"
-			/>
-			<v-textarea
-				v-model="video.description"
-				label="Description"
-				counter="20"
-				:rules="[required('description'), minLength('name', 20), maxLength('name', 20)]"
-			/>
-			<v-text-field
-				v-model="video.thumbnail"
-				label="Thumbnail URL"
-				:rules="[required('thumbnail URL')]"
-			/>
-			<v-text-field
-				v-model="video.video_url"
-				label="Video URL"
-				hint="Use Amazon s3 or similiar instead of Youtube and vimeo"
-				:rules="[required('video URL')]"
-			/>
-			<v-btn @click="saveVideo(video)" :disabled="!valid">Save Video</v-btn>
-		</v-form>
+		<VideoForm :video="video" :saveVideo="saveVideo" buttonText="Save Video"></VideoForm>
 	</v-container>
 </template>
 
 <script>
+import VideoForm from "@/components/VideoForm";
 import { mapState } from "vuex";
 export default {
-	data() {
-		return {
-			valid: false,
-			required(propertyType) {
-				return v => (v && v.length > 0) || `You must input a ${propertyType}`;
-			},
-			minLength(propertyType, minLength) {
-				return v =>
-					(v && v.length >= 5) ||
-					`${propertyType} must be at least ${minLength} characters`;
-			},
-			maxLength(propertyType, maxLength) {
-				return v =>
-					(v && v.length <= 20) ||
-					`${propertyType} must be less than ${maxLength} characters`;
-			}
-		};
+	components: {
+		VideoForm
 	},
 	computed: {
 		...mapState(["videos"]),
@@ -57,8 +18,8 @@ export default {
 		}
 	},
 	methods: {
-		async saveVideo(video) {
-			await this.$store.dispatch("editVideo", video);
+		async saveVideo() {
+			await this.$store.dispatch("editVideo", this.video);
 			this.$router.push({ name: "admin-video-list" });
 		}
 	}
--- src/views/VideoCreate.vue
+++ src/views/VideoCreate.vue
@@ -3,15 +3,7 @@
 		<h1>Video Create Page</h1>
 		<v-row>
 			<v-col md="3" cols="12">
-				<v-text-field v-model="video.name" label="Name" />
-				<v-textarea v-model="video.description" label="Description" />
-				<v-text-field v-model="video.thumbnail" label="Thumbnail URL" />
-				<v-text-field
-					v-model="video.video_url"
-					label="Video URL"
-					hint="Use Amazon s3 or similiar instead of Youtube and vimeo"
-				/>
-				<v-btn @click="createVideo">Add Video</v-btn>
+				<VideoForm :video="video" :saveVideo="createVideo" buttonText="Create Video"></VideoForm>
 			</v-col>
 			<v-col md="9" cols="12">
 				<ListVideo :video="video" />
@@ -22,9 +14,11 @@
 
 <script>
 import ListVideo from "@/components/ListVideo";
+import VideoForm from "@/components/VideoForm";
 export default {
 	components: {
-		ListVideo
+		ListVideo,
+		VideoForm
 	},
 	data() {
 		return {
