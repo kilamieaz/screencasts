c0f02d4837c5f72146a822e6b5fb30cf0a2f7065
Sultan <im.sultaan05@gmail.com>
2019-11-27 21:35:27 +0700

    navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import VideoWatch from "../views/VideoWatch.vue";
-import VideoCreate from "../views/VideoCreate.vue";
+import AdminVideoCreate from "../views/AdminVideoCreate.vue";
 import VideoByTag from "../views/VideoByTag.vue";
 import AdminVideoList from "../views/AdminVideoList.vue";
 import AdminVideoEdit from "../views/AdminVideoEdit.vue";
 import AdminUserList from "../views/AdminUserList.vue";
 import UserLogin from "../views/UserLogin.vue";
 import UserRegistration from "../views/UserRegistration.vue";
+import Admin from "../views/Admin.vue";
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,43 @@ const routes = [{
         path: "/",
         name: "home",
         component: Home
+    },
+    {
+        path: "/admin",
+        name: 'admin',
+        component: Admin,
+        beforeEnter: (to, from, next) => {
+            let currentUser = JSON.parse(window.localStorage.currentUser);
+            let accessToken = JSON.parse(window.localStorage.accessToken);
+            // currentUser.admin = true
+            if (currentUser.name && accessToken) {
+                // if (currentUser && currentUser.admin) {
+                next();
+            } else {
+                next('/');
+            }
+        },
+        children: [{
+                path: 'videos',
+                name: "admin-video-list",
+                component: AdminVideoList,
+            }, {
+                path: 'users',
+                name: "admin-user-list",
+                component: AdminUserList,
+            },
+            {
+                path: 'video/create',
+                name: "admin-video-create",
+                component: AdminVideoCreate
+            },
+            {
+                path: 'videos/:id/edit',
+                name: "admin-video-edit",
+                component: AdminVideoEdit,
+                params: true
+            },
+        ]
     },
     {
         path: '/login',
@@ -35,27 +73,6 @@ const routes = [{
         // which is lazy-loaded when the route is visited.
         component: () =>
             import( /* webpackChunkName: "about" */ "../views/About.vue")
-    },
-    {
-        path: '/admin/users',
-        name: "admin-user-list",
-        component: AdminUserList,
-    },
-    {
-        path: '/admin/videos',
-        name: "admin-video-list",
-        component: AdminVideoList,
-    },
-    {
-        path: '/admin/videos/:id/edit',
-        name: "admin-video-edit",
-        component: AdminVideoEdit,
-        params: true
-    },
-    {
-        path: '/video/create',
-        name: "video-create",
-        component: VideoCreate
     },
     {
         path: "/video/:id",
